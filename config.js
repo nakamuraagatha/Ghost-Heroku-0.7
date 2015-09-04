a0fed2dbe24d7e974043303272bbc1e29719286b
Nakamura Agatha <nakamura.agatha@gmail.com>
2015-09-05 00:25:21 +0700

    chair

--- config.js
+++ config.js
@@ -28,12 +28,12 @@ config = {
     url: process.env.HEROKU_URL,
     mail: {
       transport: 'SMTP',
-      host: 'smtp.mandrillapp.com',
+      host: 'smtp.sendgrid.net',
       options: {
-        service: 'Mandrill',
+        service: 'Sendgrid',
         auth: {
-          user: process.env.MANDRILL_USERNAME,
-          pass: process.env.MANDRILL_APIKEY
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
         }
       }
     },
